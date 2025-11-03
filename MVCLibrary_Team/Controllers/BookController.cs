8127c83e04a2769d5234f8c17ed01155b80d52c1
AlirezaMohammadi228 <s.alireza.m.r86@gmail.com>
2025-11-03 18:25:26 +0330

    Add Book Class

---
+++ MVCLibrary_Team/Controllers/BookController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCLibrary_Team.Models;
+
+namespace MVCLibrary_Team.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            bookRepository.Create(book);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            bookRepository.Delete(bookRepository.GetById(id));
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var model = bookRepository.GetById(id);
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Update(Book book, int id)
+        {
+            bookRepository.Update(book, id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVCLibrary_Team/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿namespace MVCLibrary_Team.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        private static int _nextId { get; set; } = 1;
+        public string? Title { get; set; }
+        public string? GenreId { get; set; }
+        public string? Author { get; set; }
+        public bool IsAvailable { get; set; }
+        public bool IsDeleted { get; set; }
+        public Book()
+        {
+            Id = _nextId++;
+            IsAvailable = true;
+            IsDeleted = false;
+        }
+    }
+}
