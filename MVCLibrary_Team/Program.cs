279c751c316553935df2effe8cb74bfcac9b094d
ehsan_arefzadeh <arefzaadeh@gmail.com>
2025-11-06 11:32:35 +0330

    Fix

--- MVCLibrary_Team/Program.cs
+++ MVCLibrary_Team/Program.cs
@@ -9,8 +9,7 @@ builder.Services.AddSingleton<IBaseDataModel, BaseDataModel>();
 builder.Services.AddScoped<IRepository<Book>, BookRepository>();
 builder.Services.AddScoped<IRepository<Member>, MemberRepository>();
 builder.Services.AddScoped<IRepository<Borrow>, BorrowRepository>();
-builder.Services.AddSingleton<IRepository<Borrow>, BorrowRepository>();
-
+builder.Services.AddScoped<IRepository<Category>, CategoryRepository>();
 
 builder.Services.AddDistributedMemoryCache();
 
